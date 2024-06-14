f7e34917de45e273d88f9915991bf6b2d4cceb89
hananfarizta <hananfariztaa@gmail.com>
2024-06-14 14:41:17 +0700

    Initial Commit

---
+++ MyFirstiOSApp/MyFirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstiOSAppApp.swift
+//  MyFirstiOSApp
+//
+//  Created by Hanan Farizta on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
